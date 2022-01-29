92533cce27bc69311f1595abeeb78432ffe81fcb
andrewzhng2 <andrew.zhng2@gmail.com>
2022-01-29 16:31:13 -0500

    Width 1500 to 1700

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let brushSize = 50;
 let showGrid = false;
 let fillColor = document.getElementById("color").value;
 let grid = {
-  width: 1500, // Should be 450
+  width: 1700, // Should be 450
   height: 600, // Should be 450
 };
 
