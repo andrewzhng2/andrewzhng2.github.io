93ce51a3f94cb08335a19fbeebd2c73ed3556dff
andrewzhng2 <andrew.zhng2@gmail.com>
2022-01-29 16:08:50 -0500

    500 to 1000

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let brushSize = 50;
 let showGrid = false;
 let fillColor = document.getElementById("color").value;
 let grid = {
-  width: 500, // Should be 450
+  width: 1000, // Should be 450
   height: 500, // Should be 450
 };
 
